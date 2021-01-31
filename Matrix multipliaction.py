43b860894b10ea6591d794f384eb9de973373c1b
Mullerio <66202778+Mullerio@users.noreply.github.com>
2021-01-31 17:25:43 +0100

    Add files via upload

---
+++ Matrix multipliaction.py
@@ -0,0 +1,16 @@
+
+A = [[3,0],
+     [-6,-1],
+     [5,2]]
+
+B = [[-3,4,0],
+     [1,0,-2]]
+
+result = [[sum(a*b for a,b in zip(X_row,Y_col))
+                        for Y_col in zip(*B)]
+                                for X_row in A]
+for r in result:
+    print(r)
+
+
+
