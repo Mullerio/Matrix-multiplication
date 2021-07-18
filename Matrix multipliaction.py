3ff99af9e97f4d91e4a78b30db027cd7e7060ecb
Mullerio <66202778+Mullerio@users.noreply.github.com>
2021-07-18 20:04:30 +0200

    18.07

--- Matrix multipliaction.py
+++ Matrix multipliaction.py
@@ -4,13 +4,14 @@ A = [[3,0],
      [5,2]]
 
 B = [[-3,4,0],
-     [1,0,-2]]
+     [1,0,-5]]
 
 result = [[sum(a*b for a,b in zip(X_row,Y_col))
                         for Y_col in zip(*B)]
                                 for X_row in A]
 for r in result:
     print(r)
+
 
 
 
